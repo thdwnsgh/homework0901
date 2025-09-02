e7364c3a601032ddf1a3bb36a0aace53a9277109
thdwnsgh <songjunho0202@gmail.com>
2025-09-02 18:24:13 +0900

    Add files via upload

---
+++ app105/src/main/java/com/kh/app105/App105Application.java
@@ -0,0 +1,13 @@
+package com.kh.app105;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App105Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App105Application.class, args);
+	}
+
+}
---
+++ app105/src/test/java/com/kh/app105/App105ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.app105;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App105ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
